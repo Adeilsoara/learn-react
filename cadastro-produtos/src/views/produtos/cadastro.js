1ba2b823f543b8358b55cf32b9888ed29cba1d3c
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-10 14:53:25 -0300

    Projeto CRUD - Implementação do Formulário de Produtos

--- cadastro-produtos/src/views/produtos/cadastro.js
+++ cadastro-produtos/src/views/produtos/cadastro.js
@@ -8,7 +8,59 @@ class CadastroProduto extends React.Component {
                     Cadastro de Produto
                 </div>
                 <div className='card-body'>
-                    
+                    <div className='row'>
+                        <div className='col-md-6'>
+                            <div className='form-group'>
+                                <label>Nome: </label>
+                                <input type='text' className='form-control'/>
+                            </div>
+                        </div>
+                        
+                        <div className='col-md-6'>
+                            <div className='form-group'>
+                                <label>Cod: </label>
+                                <input type='text' className='form-control'/>
+                            </div>
+                        </div>
+
+                        <div className='col-md-6'>
+
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-md-12'>
+                            <div className='form-group'>
+                                <label>Descrição</label>
+                                <textarea className='form-control'/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                    <div className='col-md-6'>
+                            <div className='form-group'>
+                                <label>Preço: </label>
+                                <input type='text' className='form-control'/>
+                            </div>
+                    </div>
+
+                    <div className='col-md-6'>
+                            <div className='form-group'>
+                                <label>Fornecedor: </label>
+                                <input type='text' className='form-control'/>
+                            </div>
+                    </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-md-1'>
+                            <button className='btn btn-success'> Salvar </button>
+                        </div>
+                        <div className='col-md-1'>
+                            <button className='btn btn-primary'> Limpar </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
