279a563f58159c564ab0963498d44a0812805288
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-10 15:53:17 -0300

    Projeto CRUD - Lendo os campos e cadastrando os dados

--- cadastro-produtos/src/views/produtos/cadastro.js
+++ cadastro-produtos/src/views/produtos/cadastro.js
@@ -1,6 +1,35 @@
 import React from 'react'
 
 class CadastroProduto extends React.Component {
+
+    state = {
+        nome: '',
+        cod: '',
+        descricao: '',
+        preco: 0,
+        fornecedor: ''
+    }
+
+    onChange = (e) =>{
+        const valor = e.target.value
+        const nomeDoCampo = e.target.name
+        this.setState({[nomeDoCampo]: valor})
+    }
+
+    onSubmit = (e) =>{
+        console.log(this.state)
+    }
+
+    limpaCampo = () => {
+        this.setState({
+            nome: '',
+            cod: '',
+            descricao: '',
+            preco: 0,
+            fornecedor: '' 
+        })
+    }
+
     render(){
         return(
             <div className='card'>
@@ -12,14 +41,22 @@ class CadastroProduto extends React.Component {
                         <div className='col-md-6'>
                             <div className='form-group'>
                                 <label>Nome: </label>
-                                <input type='text' className='form-control'/>
+                                <input type='text' 
+                                       name='nome' 
+                                       onChange={this.onChange}
+                                       value={this.state.nome} 
+                                       className='form-control'/>
                             </div>
                         </div>
                         
                         <div className='col-md-6'>
                             <div className='form-group'>
                                 <label>Cod: </label>
-                                <input type='text' className='form-control'/>
+                                <input type='text' 
+                                       name='cod' 
+                                       value={this.state.cod} 
+                                       onChange={this.onChange}
+                                       className='form-control'/>
                             </div>
                         </div>
 
@@ -32,7 +69,10 @@ class CadastroProduto extends React.Component {
                         <div className='col-md-12'>
                             <div className='form-group'>
                                 <label>Descrição</label>
-                                <textarea className='form-control'/>
+                                <textarea name='descricao' 
+                                          onChange={this.onChange}
+                                          value={this.state.descricao} 
+                                          className='form-control'/>
                             </div>
                         </div>
                     </div>
@@ -41,26 +81,35 @@ class CadastroProduto extends React.Component {
                     <div className='col-md-6'>
                             <div className='form-group'>
                                 <label>Preço: </label>
-                                <input type='text' className='form-control'/>
+                                <input type='text' 
+                                       name='preco'
+                                       onChange={this.onChange}
+                                       value={this.state.preco} 
+                                       className='form-control'/>
                             </div>
                     </div>
 
                     <div className='col-md-6'>
                             <div className='form-group'>
                                 <label>Fornecedor: </label>
-                                <input type='text' className='form-control'/>
+                                <input type='text' 
+                                       name='fornecedor'
+                                       onChange={this.onChange}
+                                       value={this.state.fornecedor} 
+                                       className='form-control'/>
                             </div>
                     </div>
                     </div>
 
                     <div className='row'>
                         <div className='col-md-1'>
-                            <button className='btn btn-success'> Salvar </button>
+                            <button onClick={this.onSubmit} className='btn btn-success'> Salvar </button>
                         </div>
                         <div className='col-md-1'>
-                            <button className='btn btn-primary'> Limpar </button>
+                            <button onClick={this.limpaCampo} className='btn btn-primary'> Limpar </button>
                         </div>
                     </div>
+
                 </div>
             </div>
         )
