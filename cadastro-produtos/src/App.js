4c2fe5c12acb0a6da3f17e30fea006fa3d58b282
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-10 11:06:00 -0300

    Projeto CRUD - Testanto se o bootswatch foi instalado

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
      
+      <div className="alert alert-dismissible alert-warning">
+        <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+        <h4 className="alert-heading">Warning!</h4>
+        <p className="mb-0"> Hello bootswatch <a href="#" class="alert-link">bootswatch</a>.</p>
+      </div>
+
     </div>
   );
 }
