d2fedcb49a11e0ec0a65a0b284366132b0c603b2
Adeilson Aragão <adeilsonaragao@hotmail.com>
2021-05-10 13:41:08 -0300

    Projeto CRUD - Implementação da Navbar

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,15 +1,12 @@
+import React from 'react';
+
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-     
-      <div className="alert alert-dismissible alert-warning">
-        <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-        <h4 className="alert-heading">Warning!</h4>
-        <p className="mb-0"> Hello bootswatch <a href="#" class="alert-link">bootswatch</a>.</p>
-      </div>
-
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
